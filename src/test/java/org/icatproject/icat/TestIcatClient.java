71c22359f8cdd27f6c0c92bc4de66b9a81c6faf2
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2026-02-17 14:13:13 +0000

    Prepare for v6.2.0

--- src/test/java/org/icatproject/icat/TestIcatClient.java
+++ src/test/java/org/icatproject/icat/TestIcatClient.java
@@ -106,7 +106,7 @@ public class TestIcatClient {
 
 	@Test
 	public void testInfo() throws Exception {
-		assertTrue(icat.getVersion().startsWith("6.1."));
+		assertTrue(icat.getVersion().startsWith("6.2."));
 	}
 
 }
